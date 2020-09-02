7a28eedee496292ef4b8c156ccae2e36db6eba7d
vladoosbobrovsky <32505948+vladoosbobrovsky@users.noreply.github.com>
2020-09-02 16:56:35 +0300

    upd IAM version

--- 05_iam.tf
+++ 05_iam.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role_policy" "ecs-service-role-policy" {
 }
 
 resource "aws_iam_instance_profile" "ecs" {
-  name = "ecs_instance_profile_prod_v03"
+  name = "ecs_instance_profile_prod_v_03"
   path = "/"
   role = aws_iam_role.ecs-host-role.name
 }
